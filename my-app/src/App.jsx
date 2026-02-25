e935f8c57b32eef9444eb6ade6577da15cb1712b
niteshjha722002 <niteshkaushaL2000a@gmail.com>
2026-02-25 20:10:32 +0530

    my-app done

---
+++ my-app/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div id="dad">
+         <h1 id="c1">Child-1</h1>
+         <h2 id="c2">Child-2</h2>
+      </div>
+      <div id="mom"></div>
+    </>
+  )
+}
+
+export default App
